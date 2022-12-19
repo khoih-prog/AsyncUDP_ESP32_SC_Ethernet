af4ba32cacd865c5ffc3da5bb653c085c9c25702
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-12-18 19:42:06 -0500

    v2.0.0 for `ESP32_S3 + LwIP W5500 / ENC28J60`
    ### Initial Releases v2.0.0
    
    1. Initial coding to port [AsyncUDP](https://github.com/espressif/arduino-esp32/tree/master/libraries/AsyncUDP) to ESP32_S3 boards using `LwIP W5500 or ENC28J60 Ethernet`
    2. Bump up to v2.0.0 to sync with [AsyncUDP v2.0.0](https://github.com/espressif/arduino-esp32/tree/master/libraries/AsyncUDP)

---
+++ examples/multiFileProject/multiFileProject.h
@@ -0,0 +1,78 @@
+/****************************************************************************************************************************
+  multiFileProject.h
+  AsyncUDP_ESP32_SC_Ethernet is a Async UDP library for the ESP32_SC_Ethernet (ESP32S2/S3/C3 + LwIP W5500 / ENC28J60)
+
+  Based on and modified from ESPAsyncUDP Library (https://github.com/me-no-dev/ESPAsyncUDP)
+  Built by Khoi Hoang https://github.com/khoih-prog/AsyncUDP_ESP32_SC_Ethernet
+  Licensed under GPLv3 license
+*****************************************************************************************************************************/
+
+// To demo how to include files in multi-file Projects
+
+#pragma once
+
+#if !( defined(ESP32) )
+  #error This code is designed for (ESP32S2/S3/C3 + LwIP W5500 or ENC28J60) to run on ESP32 platform! Please check your Tools->Board setting.
+#endif
+
+#include <Arduino.h>
+
+#define USING_W5500           true
+#define USING_ENC28J60        false
+
+#if !USING_W5500 && !USING_ENC28J60
+  #undef USING_W5500
+  #define USING_W5500           true
+#endif
+
+#define ASYNC_UDP_ESP32_SC_ETHERNET_DEBUG_PORT      Serial
+
+// Use from 0 to 4. Higher number, more debugging messages and memory usage.
+#define _ASYNC_UDP_ESP32_SC_ETHERNET_LOGLEVEL_      4
+
+//////////////////////////////////////////////////////////
+
+#if USING_W5500
+
+  // For W5500
+  // Optional values to override default settings
+  // Don't change unless you know what you're doing
+  //#define ETH_SPI_HOST        SPI3_HOST
+  //#define SPI_CLOCK_MHZ       25
+  
+  // Must connect INT to GPIOxx or not working
+  //#define INT_GPIO            4
+  
+  //#define MISO_GPIO           13
+  //#define MOSI_GPIO           11
+  //#define SCK_GPIO            12
+  //#define CS_GPIO             10
+
+  //////////////////////////////////////////////////////////
+
+#else   // #if USING_W5500
+
+  //////////////////////////////////////////////////////////
+
+  // For ENC28J60
+  // Optional values to override default settings
+  // Don't change unless you know what you're doing
+  //#define ETH_SPI_HOST        SPI3_HOST
+  //#define SPI_CLOCK_MHZ       8
+  
+  // Must connect INT to GPIOxx or not working
+  //#define INT_GPIO            4
+  
+  //#define MISO_GPIO           13
+  //#define MOSI_GPIO           11
+  //#define SCK_GPIO            12
+  //#define CS_GPIO             10
+
+  //////////////////////////////////////////////////////////
+
+#endif    // #if USING_W5500
+
+//////////////////////////////////////////////////////////
+
+// Can be included as many times as necessary, without `Multiple Definitions` Linker Error
+#include "AsyncUDP_ESP32_SC_Ethernet.hpp"
---
+++ src/AsyncUDP_ESP32_SC_Ethernet.h
@@ -0,0 +1,51 @@
+/****************************************************************************************************************************
+  AsyncUDP_ESP32_SC_Ethernet.h
+
+  AsyncUDP_ESP32_SC_Ethernet is a Async UDP library for the ESP32_SC_Ethernet (ESP32S2/S3/C3 + LwIP W5500 / ENC28J60)
+
+  Based on and modified from ESPAsyncUDP Library (https://github.com/me-no-dev/ESPAsyncUDP)
+  Built by Khoi Hoang https://github.com/khoih-prog/AsyncUDP_ESP32_SC_Ethernet
+  Licensed under GPLv3 license
+
+  Version: 2.0.0
+
+  Version Modified By   Date      Comments
+  ------- -----------  ---------- -----------
+  2.0.0   K Hoang      18/12/2022 Initial coding for ESP32_S3 using LwIP W5500 / ENC28J60. Bump up version to v2.0.0
+ *****************************************************************************************************************************/
+
+#pragma once
+
+#ifndef ASYNC_UDP_ESP32_SC_ETHERNET_H
+#define ASYNC_UDP_ESP32_SC_ETHERNET_H
+
+////////////////////////////////////////////////////
+
+#if defined(USING_W5500)
+  #if (_ASYNC_UDP_ESP32_SC_ETHERNET_LOGLEVEL_ > 3)
+    #warning USING_W5500
+  #endif
+
+  #include <WebServer_ESP32_SC_W5500.h>     // https://github.com/khoih-prog/WebServer_ESP32_SC_W5500
+#elif defined(USING_ENC28J60)
+  #if (_ASYNC_UDP_ESP32_SC_ETHERNET_LOGLEVEL_ > 3)
+    #warning USING_ENC28J60
+  #endif
+
+  #include <WebServer_ESP32_SC_ENC.h>       // https://github.com/khoih-prog/WebServer_ESP32_SC_ENC
+#else
+  #if (_ASYNC_UDP_ESP32_SC_ETHERNET_LOGLEVEL_ > 3)
+    #warning Default to USING_W5500
+  #endif
+
+  #include <WebServer_ESP32_SC_W5500.h>     // https://github.com/khoih-prog/WebServer_ESP32_SC_W5500
+#endif
+
+////////////////////////////////////////////////
+
+#include "AsyncUDP_ESP32_SC_Ethernet.hpp"
+#include "AsyncUDP_ESP32_SC_Ethernet_Impl.h"
+
+////////////////////////////////////////////////
+
+#endif    //ASYNC_UDP_ESP32_SC_ETHERNET_H
